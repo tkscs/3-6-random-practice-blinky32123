87f8d80f3d772dfcc54f92b7e1318c20dc625827
blinky32123 <jkrepner2028@kehillahstudent.org>
2026-02-13 11:49:55 -0800

    hii

--- q1.py
+++ q1.py
@@ -1,13 +1,29 @@
+import random
 def spin_twister_spinner():
+  colors=["red","green","yellow","blue"]
+  sides=["left", "'right"]
+  appendage=["hand", "foot"]
+  
+  
+  print(random.choices(colors, k=1))
+  print(random.choices(sides, k=1))
+  print(random.choices(appendage, k=1))
+  
+
+  
+  
+
+
   """
   Returns a list with a random color, side, and appendage
-  
+
   colors: "red", "green", "yellow", or "blue"
   sides: "left" or "right"
   appendage: "hand" or "foot"
   """
   #YOUR CODE HERE
   return
+
 
 # Here's the function call. This should print a random assortment of twister commands
 for _ in range(10):
--- q3.py
+++ q3.py
@@ -1,7 +1,14 @@
+import random
 suits = "♠♥♣♦"
 values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-deck = []
+values=print(random.choices(values, k=5))
+suits=print(random.choices(suits, k=5))
+deck = "f[values, suits]"
+print(deck)
+
+
+
 # Make a deck of cards
 # `deck` should be a list of strings with a value and a suit, e.g. "4♣"
 
@@ -9,3 +16,10 @@ deck = []
 
 # sample a hand of 5 cards and print it out
 # (WITHOUT replacement -- no repeats!)
+
+# for i in range(5):
+#     values=random.choices(values, k=1)
+#     suits=random.choices(suits, k=1)
+#     deck = f"{values, suits}"
+#     print(deck)
+    
